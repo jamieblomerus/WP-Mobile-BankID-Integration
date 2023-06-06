ba05beab49f890645870b85961d0675c49039ac2
Jamie Blomerus <jamie.blomerus@protonmail.com>
2023-06-06 11:17:23 +0200

    Fixed small bug

--- includes/integrations/load.php
+++ includes/integrations/load.php
@@ -1,5 +1,8 @@
 <?php
 add_action('init', function() {
+    if (!function_exists('is_plugin_active')) {
+        return;
+    }
     if (is_plugin_active('woocommerce/woocommerce.php')) {
         include_once WP_BANKID_PLUGIN_DIR . 'includes/integrations/woocommerce.php';
     }
