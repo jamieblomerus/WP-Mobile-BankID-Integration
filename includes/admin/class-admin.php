376337de9d7b3b7a9e71f2cf3c263370ec138186
Jamie Blomerus <jamie.blomerus@protonmail.com>
2023-06-06 11:17:42 +0200

    Corrected link to WooCommerce settings

--- includes/admin/class-admin.php
+++ includes/admin/class-admin.php
@@ -197,7 +197,7 @@ class Admin {
                     <h2 class="wp-bankid-integration__title">WooCommerce</h2>
                     <p class="wp-bankid-integration__description">WooCommerce is the most popular e-commerce platform for WordPress. With WP BankID by Webbstart you can perform age checks using BankID.</p>
                     <?php if (is_plugin_active('woocommerce/woocommerce.php')) : ?>
-                        <a href="<?php echo esc_url(admin_url('admin.php?page=wc-settings&tab=bankid')); ?>" class="button button-primary"><?php esc_html_e('Go to settings', 'wp-bankid'); ?></a>
+                        <a href="<?php echo esc_url(admin_url('admin.php?page=wc-settings&tab=advanced&section=wp_bankid')); ?>" class="button button-primary"><?php esc_html_e('Go to settings', 'wp-bankid'); ?></a>
                     <?php elseif (file_exists(WP_PLUGIN_DIR . '/woocommerce/woocommerce.php')):
                         // Generate _wpnonce
                         $nonce = wp_create_nonce('activate-plugin_woocommerce/woocommerce.php');
