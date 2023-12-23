49821389e2b2ba5f82b818a76cad79bc774473ac
Jamie Blomerus <jamie.blomerus@protonmail.com>
2023-12-23 17:54:40 +0100

    Reviewed plugin file and improved WPCS-compliance

--- mobile-bankid-integration.php
+++ mobile-bankid-integration.php
@@ -1,4 +1,4 @@
-<?php
+<?php // PHPCS:Ignore
 /*
 Plugin Name: Mobile BankID Integration
 Description: A plugin that allows you to integrate Mobile BankID with your WordPress site.
@@ -8,48 +8,62 @@ Author: Jamie Blomerus
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
-    die;
+	die;
 }
 
-// Define constants
+// Define constants.
 define( 'MOBILE_BANKID_INTEGRATION_VERSION', 'Indev' );
 define( 'MOBILE_BANKID_INTEGRATION_PLUGIN_FILE', __FILE__ );
 define( 'MOBILE_BANKID_INTEGRATION_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'MOBILE_BANKID_INTEGRATION_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-if (!class_exists("mobile_bankid_integration")) {
-    class mobile_bankid_integration
-    {
-        public function __construct()
-        {
-            // Composer autoload
-            require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'vendor/autoload.php';
+if ( ! class_exists( 'mobile_bankid_integration' ) ) {
 
-            // Load plugin files
-            $this->load_plugin_files();
+	/**
+	 * Main plugin class.
+	 */
+	class Mobile_Bankid_Integration {
 
-            // Add link in plugin list
-            add_filter('plugin_action_links_' . plugin_basename(MOBILE_BANKID_INTEGRATION_PLUGIN_FILE), [$this, 'plugin_list_link']);
-        }
+		/**
+		 * Class constructor.
+		 */
+		public function __construct() {
+			// Composer autoload.
+			require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'vendor/autoload.php';
 
-        private function load_plugin_files()
-        {
-            require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/class-core.php';
-            require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/class-activation.php';
-            require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/settings/class-setup.php';
-            require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/settings/class-api.php';
-            require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/settings/class-user.php';
-            require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/wp-login/class-api.php';
-            require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/wp-login/class-login.php';
-            require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/admin/class-admin.php';
-            require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/integrations/load.php';
-        }
+			// Load plugin files.
+			$this->load_plugin_files();
 
-        public function plugin_list_link($links) {
-            $setup_link = '<a href="admin.php?page=mobile-bankid-integration">'.esc_html__("Settings", "mobile-bankid-integration").'</a>';
-            array_unshift($links, $setup_link);
-            return $links;
-        }
-    }
-    new mobile_bankid_integration;
+			// Add link in plugin list.
+			add_filter( 'plugin_action_links_' . plugin_basename( MOBILE_BANKID_INTEGRATION_PLUGIN_FILE ), array( $this, 'plugin_list_link' ) );
+		}
+
+		/**
+		 * Load plugin files.
+		 */
+		private function load_plugin_files() {
+			require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/class-core.php';
+			require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/class-activation.php';
+			require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/settings/class-setup.php';
+			require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/settings/class-api.php';
+			require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/settings/class-user.php';
+			require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/wp-login/class-api.php';
+			require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/wp-login/class-login.php';
+			require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/admin/class-admin.php';
+			require_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/integrations/load.php';
+		}
+
+		/**
+		 * Add link to settings page in plugin list.
+		 *
+		 * @param array $links Array of links.
+		 * @return array
+		 */
+		public function plugin_list_link( $links ) {
+			$setup_link = '<a href="admin.php?page=mobile-bankid-integration">' . esc_html__( 'Settings', 'mobile-bankid-integration' ) . '</a>';
+			array_unshift( $links, $setup_link );
+			return $links;
+		}
+	}
+	new Mobile_Bankid_Integration();
 }
