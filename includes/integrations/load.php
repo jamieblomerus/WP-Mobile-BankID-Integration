305c4332fb127fb20eb68418eccee3be13df0825
Jamie Blomerus <jamie.blomerus@protonmail.com>
2023-12-26 09:57:11 +0100

    Fixed all WPCS violations in integrations/load.php

--- includes/integrations/load.php
+++ includes/integrations/load.php
@@ -1,9 +1,12 @@
 <?php
-add_action('init', function() {
-    if (!function_exists('is_plugin_active')) {
-        return;
-    }
-    if (is_plugin_active('woocommerce/woocommerce.php')) {
-        include_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/integrations/woocommerce.php';
-    }
-});
+add_action(
+	'init',
+	function () {
+		if ( ! function_exists( 'is_plugin_active' ) ) {
+			return;
+		}
+		if ( is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
+			include_once MOBILE_BANKID_INTEGRATION_PLUGIN_DIR . 'includes/integrations/woocommerce.php';
+		}
+	}
+);
