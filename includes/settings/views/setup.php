2b8dcdc0e1c6aaf2ecb078bfee1c7ad33aebde4d
Jamie Blomerus <jamie.blomerus@protonmail.com>
2024-01-16 12:06:04 +0100

    Used PHPCBF to fix 253 WPCS Errors in /includes/settings/views/

--- includes/settings/views/setup.php
+++ includes/settings/views/setup.php
@@ -1,60 +1,88 @@
 <?php
 // Setup view on activation.
 
-$step = isset($_GET['step']) ? $_GET['step'] : 1;
-$step = intval($step);
+$step = isset( $_GET['step'] ) ? $_GET['step'] : 1;
+$step = intval( $step );
 ?>
 <div id="wizard-modal">
-    <div id="wizard-modal-content">
-        <div id="wizard-modal-content-inner">
-            <h2><?php esc_html_e('Are you sure?', 'mobile-bankid-integration'); ?></h2>
-            <p id="wizard-modal-confirmation-text"></p>
-        </div>
-        <hr>
-        <div id="wizard-modal-footer">
-            <button class="button button-primary" id="wizard-modal-abort"><?php esc_html_e('Abort', 'mobile-bankid-integration'); ?></button>
-            <button class="button button-secondary" onclick="confirmconfirmation()" id="wizard-modal-confirm"><?php esc_html_e('Confirm', 'mobile-bankid-integration'); ?></button>
-        </div>
-    </div>
+	<div id="wizard-modal-content">
+		<div id="wizard-modal-content-inner">
+			<h2><?php esc_html_e( 'Are you sure?', 'mobile-bankid-integration' ); ?></h2>
+			<p id="wizard-modal-confirmation-text"></p>
+		</div>
+		<hr>
+		<div id="wizard-modal-footer">
+			<button class="button button-primary" id="wizard-modal-abort"><?php esc_html_e( 'Abort', 'mobile-bankid-integration' ); ?></button>
+			<button class="button button-secondary" onclick="confirmconfirmation()" id="wizard-modal-confirm"><?php esc_html_e( 'Confirm', 'mobile-bankid-integration' ); ?></button>
+		</div>
+	</div>
 </div>
 <div class="wizard">
-    <div class="steps">
-        <ol>
-            <li <?php if ($step > 1) {?>class="done"<?php } else {?>class="active"<?php } ?>>
-                <span class="title"><?php esc_html_e('Welcome', 'mobile-bankid-integration') ?></span>
-            </li>
-            <li <?php if ($step > 2) {?>class="done"<?php } elseif ($step == 2) {?>class="active"<?php } ?>>
-                <span class="title"><?php esc_html_e('Configuration', 'mobile-bankid-integration') ?></span>
-            </li>
-            <li <?php if ($step > 3) {?>class="done"<?php } elseif ($step == 3) {?>class="active"<?php } ?>>
-                <span class="title"><?php esc_html_e('Settings', 'mobile-bankid-integration') ?></span>
-            </li>
-            <li <?php if ($step == 4) {?>class="active"<?php } ?>>
-                <span class="title"><?php esc_html_e('Finish', 'mobile-bankid-integration') ?></span>
-            </li>
-        </ol>
-    </div><br>
+	<div class="steps">
+		<ol>
+			<li 
+			<?php
+			if ( $step > 1 ) {
+				?>
+class="done"
+				<?php
+			} else {
+				?>
+				class="active"<?php } ?>>
+				<span class="title"><?php esc_html_e( 'Welcome', 'mobile-bankid-integration' ); ?></span>
+			</li>
+			<li 
+			<?php
+			if ( $step > 2 ) {
+				?>
+class="done"
+				<?php
+			} elseif ( $step == 2 ) {
+				?>
+class="active"<?php } ?>>
+				<span class="title"><?php esc_html_e( 'Configuration', 'mobile-bankid-integration' ); ?></span>
+			</li>
+			<li 
+			<?php
+			if ( $step > 3 ) {
+				?>
+class="done"
+				<?php
+			} elseif ( $step == 3 ) {
+				?>
+class="active"<?php } ?>>
+				<span class="title"><?php esc_html_e( 'Settings', 'mobile-bankid-integration' ); ?></span>
+			</li>
+			<li 
+			<?php
+			if ( $step == 4 ) {
+				?>
+class="active"<?php } ?>>
+				<span class="title"><?php esc_html_e( 'Finish', 'mobile-bankid-integration' ); ?></span>
+			</li>
+		</ol>
+	</div><br>
 
-    <div id="wizard-content" step="<?php echo esc_attr($step); ?>">
-        <?php
-        switch ($step) {
-            case 1:
-                include_once 'setup-welcome.php';
-                break;
-            case 2:
-                include_once 'setup-configuration.php';
-                break;
-            case 3:
-                include_once 'setup-settings.php';
-                break;
-            case 4:
-                include_once 'setup-finish.php';
-                break;
-            default:
-                include_once 'setup-welcome.php';
-                break;
-        }
-        ?>
-    </div>
-    <p class="footer-info"><?php esc_html_e('Mobile BankID Integration version: ', 'mobile-bankid-integration'); ?> <?php echo esc_html(MOBILE_BANKID_INTEGRATION_VERSION); ?></p>
+	<div id="wizard-content" step="<?php echo esc_attr( $step ); ?>">
+		<?php
+		switch ( $step ) {
+			case 1:
+				include_once 'setup-welcome.php';
+				break;
+			case 2:
+				include_once 'setup-configuration.php';
+				break;
+			case 3:
+				include_once 'setup-settings.php';
+				break;
+			case 4:
+				include_once 'setup-finish.php';
+				break;
+			default:
+				include_once 'setup-welcome.php';
+				break;
+		}
+		?>
+	</div>
+	<p class="footer-info"><?php esc_html_e( 'Mobile BankID Integration version: ', 'mobile-bankid-integration' ); ?> <?php echo esc_html( MOBILE_BANKID_INTEGRATION_VERSION ); ?></p>
 </div>
