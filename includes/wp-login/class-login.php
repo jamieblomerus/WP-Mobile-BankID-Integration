779b402de18f47ef3094f52ce06ae8c27a71480c
Jamie Blomerus <jamie.blomerus@protonmail.com>
2024-10-31 09:31:05 +0100

    Made screen reader help hideable and qr enlargeability optional

--- includes/wp-login/class-login.php
+++ includes/wp-login/class-login.php
@@ -47,41 +47,76 @@ class Login {
 
 	/**
 	 * Add login container to login page.
+	 * 
+	 * @since 1.4.1 Added filters.
 	 *
+	 * @param string $dom_element The DOM element to use for the container.
+	 * @param array  $class       Additional classes to add to the container.
 	 * @return void
 	 */
 	public function login_container($dom_element = 'form', $class = array()) {
 		if ( ! in_array( $dom_element, array( 'form', 'div', 'section' ), true ) ) {
 			$dom_element = 'form';
 		}
+
+		/**
+		 * Filter whether the QR code should be enlargeable.
+		 * 
+		 * @since 1.4.1
+		 */
+		$qr_enlargeable = (bool) apply_filters( 'mobile_bankid_integration_login_qr_enlargeable', true );
 		?>
 		<<?php echo esc_attr( $dom_element ); ?> id="bankid-login-container" class="<?php echo esc_attr( implode( ' ', $class ) ); ?>" style="display: none;">
 			<h2 id="bankid-login-h2"><?php echo apply_filters( 'mobile_bankid_integration_login_heading', esc_html__( 'Login with BankID', 'mobile-bankid-integration' ) ) ?></h2>
 			<p id="bankid-status"><?php echo apply_filters( 'mobile_bankid_integration_login_qr_instructions', esc_html__( 'Scan the QR code with your Mobile BankID app.', 'mobile-bankid-integration' ) ) ?></p>
-			<div id="bankid-qr-code-container" role="button" aria-label="<?php esc_attr_e( 'Enlarge the QR code', 'mobile-bankid-integration' ) ?>">
+			<div id="bankid-qr-code-container" <?php echo $qr_enlargeable ? 'enlargeable role="button"' : ''; ?> <?php echo $qr_enlargeable ? 'aria-label="' . apply_filters( 'mobile_bankid_integration_login_qr_click_to_enlarge', esc_attr__( 'Enlarge the QR code', 'mobile-bankid-integration' ) ) . '"' : ''; ?>>
 				<img id="bankid-qr-code" src="" alt="<?php echo apply_filters( 'mobile_bankid_integration_login_qr_alt', esc_attr__( 'QR code', 'mobile-bankid-integration' ) ) ?>">
 				<div id="bankid-qr-code-loading" aria-hidden="true">
 					<div class="spinner"></div>
 				</div>
 				<div id="bankid-qr-code-timeleft" aria-hidden="true"></div>
 			</div>
-			<div class="accordion screen-reader-accordion" role="region">
-				<button class="accordion-button" aria-expanded="false" aria-controls="bankid-screen-reader-help"><?php esc_html_e( 'If you use a screen reader', 'mobile-bankid-integration' ) ?><span class="icon" aria-hidden="true"></span></button>
-				<div id="bankid-screen-reader-help" class="accordion-content">
-					<p><?php esc_html_e( 'The most common problem is that the QR code doesn\'t fit on the screen. Please try to:', 'mobile-bankid-integration' ) ?></p>
-					<ul>
-						<li><?php esc_html_e( 'Ensure that the screen is switched on and Screen Curtain or similar functions are switched off.', 'mobile-bankid-integration' ) ?></li>
-						<li><?php esc_html_e( 'Zoom out in your browser by pressing Ctrl or Cmd-0.', 'mobile-bankid-integration' ) ?></li>
-						<li><?php esc_html_e( 'Zoom out with magnification tools such as ZoomText.', 'mobile-bankid-integration' ) ?></li>
-						<li><?php esc_html_e( 'Ensure the browser window is maximized.', 'mobile-bankid-integration' ) ?></li>
-						<li><?php esc_html_e( 'Hold your phone in portrait mode at an arm\'s lengths distance from the screen when you scan the QR code.', 'mobile-bankid-integration' ) ?></li>
-					</ul>
-					<p><?php esc_html_e( 'You can also click on the QR code above for it to be displayed bigger.', 'mobile-bankid-integration' ) ?></p>
-				</div>
-			</div>
+			<?php
+			/**
+			 * Fires after the QR code is displayed.
+			 * 
+			 * @since 1.4.1
+			 */
+			do_action( 'mobile_bankid_integration_login_after_qr_code' );
+
+			// Screen reader accordion.
+			$sr_help_callback = apply_filters( 'mobile_bankid_integration_login_screen_reader_help', [ $this, 'screen_reader_help' ] );
+			if ( is_callable( $sr_help_callback ) ) {
+				call_user_func( $sr_help_callback );
+			}
+			?>
 			<a href="#" id="cancel_bankid" class="button wp-element-button"><?php echo apply_filters( 'mobile_bankid_integration_login_cancel', esc_html__( 'Cancel', 'mobile-bankid-integration' ) ) ?></a>
 			<a target="_blank" id="open_bankid" href="#" class="button wp-element-button" style="margin-left: 5px;"><?php echo apply_filters( 'mobile_bankid_integration_login_start_app', esc_html__( 'Start the BankID app', 'mobile-bankid-integration' ) ) ?></a>
 		</<?php echo esc_attr( $dom_element ); ?>>
+		<?php
+	}
+
+	/**
+	 * Add screen reader help to login page.
+	 *
+	 * @return void
+	 */
+	public function screen_reader_help() {
+		?>
+		<div class="accordion screen-reader-accordion" role="region">
+			<button class="accordion-button" aria-expanded="false" aria-controls="bankid-screen-reader-help"><?php esc_html_e( 'If you use a screen reader', 'mobile-bankid-integration' ) ?><span class="icon" aria-hidden="true"></span></button>
+			<div id="bankid-screen-reader-help" class="accordion-content">
+				<p><?php esc_html_e( 'The most common problem is that the QR code doesn\'t fit on the screen. Please try to:', 'mobile-bankid-integration' ) ?></p>
+				<ul>
+					<li><?php esc_html_e( 'Ensure that the screen is switched on and Screen Curtain or similar functions are switched off.', 'mobile-bankid-integration' ) ?></li>
+					<li><?php esc_html_e( 'Zoom out in your browser by pressing Ctrl or Cmd-0.', 'mobile-bankid-integration' ) ?></li>
+					<li><?php esc_html_e( 'Zoom out with magnification tools such as ZoomText.', 'mobile-bankid-integration' ) ?></li>
+					<li><?php esc_html_e( 'Ensure the browser window is maximized.', 'mobile-bankid-integration' ) ?></li>
+					<li><?php esc_html_e( 'Hold your phone in portrait mode at an arm\'s lengths distance from the screen when you scan the QR code.', 'mobile-bankid-integration' ) ?></li>
+				</ul>
+				<p><?php esc_html_e( 'You can also click on the QR code above for it to be displayed bigger.', 'mobile-bankid-integration' ) ?></p>
+			</div>
+		</div>
 		<?php
 	}
 
